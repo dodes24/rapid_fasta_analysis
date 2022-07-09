85fb018c64c65bc2e7ee82a62440c4be7dde5976
dodes24 <fulop.jozef1@gmail.com>
2022-07-09 21:22:50 +0200

    GC content calculation in number of nucleotides and percentage added.

--- analysis.py
+++ analysis.py
@@ -1,5 +1,5 @@
-!/bin/python
--*- coding: utf-8 -*-
+#!/bin/python
+# -*- coding: utf-8 -*-
 """
 This program takes a FASTA file and returns various analysis of the sequences.
 """
@@ -18,23 +18,26 @@ import seaborn as sns
 
 from Bio import SeqIO
 
-fasta_file = input("Enter the name of the FASTA file: ") # input file using terminal
+fasta_file = input("Enter the name of the FASTA file: ")  # input file using terminal
+
+
 def parse_fasta(fasta_file):
     '''
     This function takes a FASTA file and returns the ID, sequence, and length of the sequences.
     :param fasta_file:
-    :return: seq_id, sequences, lenghts
+    :return: seq_id, sequences, lengths
     '''
     seq_id = []
     sequences = []
     lengths = []
-    # created lists for the IDs, sequences, and lengthsq
+    # created lists for the IDs, sequences, and lengths
     for record in SeqIO.parse(fasta_file, "fasta"):
         seq_id.append(record.id)
         sequences.append(record)
-        lengths.append(len(record)) # added the ids sequences and lengths of the sequence to the list
+        lengths.append(len(record))  # added the ids sequences and lengths of the sequence to the list
 
-    return seq_id, sequences, lengths # now works fine with one sequence in the file. Input file will be genomic sequence.
+    return seq_id, sequences, lengths  # now works fine with one sequence in the file. Input file will be genomic sequence.
+
 
 # print(parse_fasta(fasta_file))
 
@@ -42,10 +45,29 @@ def mRNA(fasta_file):
     '''
     This function makes mRNA from the DNA sequence.
     '''
-    seq_id, sequences, lenghts = parse_fasta(fasta_file) # calling the function to get the IDs, sequences, and lengths
-    mRNA_seq = [] # created a list for the mRNA sequences
+    seq_id, sequences, lengths = parse_fasta(fasta_file)  # calling the function to get the IDs, sequences, and lengths
+    mRNA_seq = []  # created a list for the mRNA sequences
     for record in sequences:
-        mRNA_seq.append(str(record.seq).replace("T", "U")) # replacing the T with U in the DNA sequence
+        mRNA_seq.append(str(record.seq).replace("T", "U"))  # replacing the T with U in the DNA sequence
     return mRNA_seq
 
+
 # print(mRNA(fasta_file))
+
+def gc_content(fasta_file):
+    '''
+    This function returns the GC content of the sequences.
+    '''
+    global all_nucleotides, gc_percent
+    seq_id, sequences, lengths = parse_fasta(fasta_file)  # calling the function to get the IDs, sequences, and lengths
+    gc_cont = []  # created a list for the GC content
+    for record in sequences:
+        gc_content.append(str(record.seq).count("G") + str(record.seq).count("C"))  # counting the number of G and C 
+        # in the DNA sequence 
+        all_nucleotides = str(record.seq).count("A") + str(record.seq).count("T") + str(record.seq).count("G") + str(
+            record.seq).count("C")  # counting the number of A, T, G, and C in the DNA sequence
+        gc_percent = round((gc_cont[0] / all_nucleotides) * 100, 2)  # calculating the GC content in percent
+    return gc_cont, all_nucleotides, gc_percent # returning the GC content, all nucleotides, and GC percent
+
+
+#print(gc_content(fasta_file))
