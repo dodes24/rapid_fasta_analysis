bd86a0fbcd4fa2edf0d5b08de4c9224b0d3dd6b9
dodes24 <fulop.jozef1@gmail.com>
2022-07-11 04:43:09 +0200

    done: sequence getter orf_finder - innfo about orfs orf_nucleotides - nucleotides of orfs list_seq_nucleotides - list of nucleotides of orf sequences translate_sequence - translate nucleotides to amino acids, with wrapper for translate function genes_to_protein - uses wrapper and translate to protein
    to do : somehow iterate trught the list of nested dictionaries, and each dictionary
    will be header of protein sequence in fasta file

--- analysis.py
+++ analysis.py
@@ -20,10 +20,11 @@ from Bio.SeqRecord import SeqRecord
 from Bio import SeqIO
 from Bio.SeqUtils import GC
 from Bio.Seq import transcribe
+from orffinder import orffinder
 
 
 # fasta_file = input("Enter the name of the FASTA file: ")  # input file using terminal
-fasta_file = "genomic.fna"
+fasta_file = "tuberculosis_genomic.fna"
 
 
 def parse_fasta(fasta_file):
@@ -44,7 +45,7 @@ def parse_fasta(fasta_file):
     return seq_id, sequences, lengths  # now works fine with one sequence in the file. Input file will be genomic sequence.
 
 
-print(parse_fasta(fasta_file))
+#print(parse_fasta(fasta_file))
 
 
 def gc_content(fasta_file):
@@ -62,10 +63,9 @@ def gc_content(fasta_file):
 print(gc_content(fasta_file))
 
 
-def transcribe_to_mrna(fasta_file):
+def transcribe_to_mrna():
     """
     This function transcribes the DNA sequence to mRNA.
-    :param: fasta_file
     :return: mrna
     """
     seq_id, sequences, lengths = parse_fasta(fasta_file)
@@ -74,22 +74,126 @@ def transcribe_to_mrna(fasta_file):
         return mrna
 
 
-print(transcribe_to_mrna(fasta_file))
+# print(transcribe_to_mrna(fasta_file))
+
+def get_sequence():
+    sequence_to_orf = SeqIO.read(fasta_file, "fasta")
+    return sequence_to_orf
+
+def orf_finder():
+    '''
+    This function finds the ORFs in the mRNA sequence.
+    :param: mrna
+    :return: orfs
+    '''
+    orfs = orffinder.getORFs(get_sequence(), minimum_length=75, remove_nested=False)  # remove_nested=True)
+    return orfs
+
+
+print(orf_finder())
+
+
+def orf_nucleotides():
+    '''
+    This function returns the nucleotides of the ORFs.
+    '''
+    orf_nucleotides = orffinder.getORFNucleotides(get_sequence(), return_loci=False)
+    return orf_nucleotides
+'''
+    for x in orf_nucleotides:
+        if (len(str(x))) % 3 != 0:
+           printed = print(len(x))
+        #printed = print(len(str((x))))
+    return printed
+'''
+#print(orf_nucleotides()[0])
+
+def list_seq_nucleotides():
+    '''
+    This function returns list of the nucleotides of the ORFs.
+    '''
+    seq_nucleotides = []
+    for orfs in orf_nucleotides():
+        seq_nucleotides.append(str(orfs))
+    return seq_nucleotides
+
+#print(list_seq_nucleotides())
 
 
 
-# next step is to get the amino acid sequence, plots
+def translate_sequence(input_seq):
+    """Wrapper for Biopython translate function.  Bio.Seq.translate will complain if input sequence is
+    not a mulitple of 3.  This wrapper function passes an acceptable input to Bio.Seq.translate in order to
+    avoid this warning."""
+
+    trailing_bases = len(str(input_seq)) % 3
+
+    if trailing_bases:
+        input_seq = ''.join([str(input_seq), 'NN']) if trailing_bases == 1 else ''.join([str(input_seq), 'N'])
+
+    output_seq = Seq.translate(input_seq, to_stop=True)
+
+    if trailing_bases:
+        # remove last residue if input needed to be extended because of trailing bases
+        output_seq = output_seq[:-1]
+
+    return output_seq
+
+
+def genes_to_protein():
+    '''
+    This function translates the ORFs to proteins.
+    returns proteins in a list of Seq objects.
+    '''
+    proteins = []
+    for orfs in orf_nucleotides():
+        translated = translate_sequence(orfs)
+        proteins.append(translated)
+    return proteins
+
+
+#print(genes_to_protein())
+
+
+def freeze_iterate(d):
+    for i in orf_finder():
+        for key, value in i.items():
+            yield key, value
+    return d
+
+print(freeze_iterate(orf_finder()))
+
+
+def dictionary_for_fasta():
+    '''
+    This function creates a dictionary of the ORFs info and their proteins.
+    '''
+    loci_list = freeze(orf_finder())
+    nucleotide_list = list_seq_nucleotides()
+    protein_list = genes_to_protein()
+    #loci_list = orf_finder()
+    dictionary = dict(zip(loci_list, protein_list))
+    return dictionary
+    pass
+#print(dictionary_for_fasta())
 
 
 
 
 
 
+"""
+def export_found_proteins(infile=genes_to_protein(), outfile):
+    '''
+    This function exports the proteins to a file.
+    '''
+    for line in infile:
+        if line.startswith('>'):
+            header = line.lststrip('>')
+            outfile.write(header)
+        else:
 
-
-
-
-
+"""
 
 """
 def gc_content(fasta_file):
